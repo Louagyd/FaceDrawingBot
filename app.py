97caf0d4361ccb87c1b623ce020807e8d3abf73a
Ali Golmakani <Golmakani77@yahoo.com>
2019-12-14 01:18:08 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-#import tensorflow as tf
-import imageio
+import tensorflow as tf
+#import imageio
 from PIL import Image
+import numpy as np
 #import cv2
 if __name__ == '__main__':   
     print("kiiir")
